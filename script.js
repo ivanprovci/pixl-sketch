b66fb2ab8101db73b410fba84b9d53b45529b16d
ivanprovci <ivanprovci@yahoo.com>
2024-01-29 12:33:36 -0800

    Added grid + draw

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const colorPicker = document.querySelector('#colorPicker')
-const colorPickerOverlay = document.querySelector('.circle')
-
+let color = colorPicker.value
 colorPicker.addEventListener('input', (e) => {
-    console.log(e.target.value)
-    colorPickerOverlay.style.backgroundColor = e.target.value
+    color = e.target.value
 })
+
+const grid = document.querySelector(".grid")
+const GRID_SIZE = 32
+const GRID_WIDTH = grid.clientWidth - parseFloat(getComputedStyle(grid, null).getPropertyValue('padding'))*2
+for(let i = 0; i < GRID_SIZE; i++){
+    for(let j = 0; j < GRID_SIZE; j++){
+        const pixel = document.createElement('div')
+        pixel.classList.add('pixel')
+        pixel.style.width = (GRID_WIDTH / GRID_SIZE) + "px"
+        pixel.style.height = (GRID_WIDTH / GRID_SIZE) + "px"
+        grid.appendChild(pixel)
+
+        //mouse1 is already being held when it enters the pixel
+        pixel.addEventListener('mouseover', e => {
+            if(e.buttons === 1) {
+                if(e.target.style.backgroundColor !== color){
+                    e.target.style.backgroundColor = color
+                }
+            }
+        })
+        pixel.addEventListener('mousedown', e => {
+            e.target.style.backgroundColor = color
+        })
+        
+        //disables dragging the pixel element bug
+        pixel.addEventListener('dragstart', e => {
+            e.preventDefault()
+        })
+
+        
+    }
+}
